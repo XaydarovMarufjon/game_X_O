85f8611c6272fccd75309c3233441036adc6c3a3
XaydarovMarufjon <marufjankhaydarov@gmail.com>
2023-05-16 14:02:57 +0500

    sound add

--- game.js
+++ game.js
@@ -22,6 +22,10 @@ const game = {
     ]
 }
 
+    const audio1 = new Audio('./sounds/Salom hurmatli foyda.mp3');
+    audio1.play();
+
+
 document.addEventListener('click', event => { /// document.addEventListner butun saxifaga hodisa qoshadi
     const target = event.target     // tanlab olingan katak yani sichqoncha bilan bosilgan
     const isCell = target.classList.contains('grid-cell') // tanlab olingan katakda shu class bor yoki yoqligi tekshiradi 
@@ -40,16 +44,16 @@ document.addEventListener('click', event => { /// document.addEventListner butun
       game.xTurn = !game.xTurn // xTurni holatini ozgartiradi yanni ture bo'lsa false , false bo'lsa Truega 
     }
 
-
     if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) { //  disabled clasi yoq .grid-cell  clasni sanaydi !true / bor yoki yoq / class qolmagan pyt truega aylanadi yani  !false
+        
     document.querySelector('.game-over').classList.add('visible') // Har safar katakka belgi qo'shsak, uni o'chirib qo'yamiz
-    document.querySelector('.game-over-text').textContent = 'O\'yin durrang yakunlandi!'
+    document.querySelector('.game-over-text').textContent = 'O\'yin yakunlandi!'
     }
-
+  
    game.winningStates.forEach(winningState => {
     const xWins = winningState.every(state => game.xState.includes(state)) // xWins massivini tekshiradi  true / false 
     const oWins = winningState.every(state => game.oState.includes(state))// yWins massivini tekshiradi   true / false 
-
+    
     if (xWins || oWins) {  
         document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled')) // true holat kelganida oyin toxtaydi 
         document.querySelector('.game-over').classList.add('visible')        // toxtauvchi oyna ishga tushadi 
@@ -57,11 +61,22 @@ document.addEventListener('click', event => { /// document.addEventListner butun
             ? 'X g\'olib!'                                                   // Ekranga X lar g'olib dgan yozuv chiqadi Yoki
             : 'O g\'olib!'                                                   // Ekranga Y lar g'olib dgan yozuv chiqadi
     } 
+    if (xWins) {
+        const audio = new Audio('./sounds/Ikslar uyinda yutdi.mp3');
+        audio.play();
+    }
+    if(oWins){
+        const audio = new Audio('./sounds/no llar uyinda yutdi.mp3');
+        audio.play();
+    }
+
    })
 })
 
 
 document.querySelector('.restart').addEventListener('click', () => {     // restart tugmasi bosilganda Obyekt yangilanadi 
+    const audio = new Audio('./sounds/uyinni Qayta boshlas.mp3');
+        audio.play();
     document.querySelector('.game-over').classList.remove('visible')        // yakunlovchi oyna yopiladi 
     document.querySelectorAll('.grid-cell').forEach(cell => {            // .grid-cell  clasi bor hamma classni tanlaydi 
         cell.classList.remove('disabled', 'x', 'o')                    // x , o va disabled classlari olib tashlanadi 
